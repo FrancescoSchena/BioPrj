c4913219f1bbe229432c9e29c86f28709306948e
Francesco Schena <francesco@schena.eu>
2021-04-03 23:57:28 +0200

    General data viz improvements

--- plot.py
+++ plot.py
@@ -6,9 +6,12 @@ import seaborn as sn
 import matplotlib.pyplot as plt
 from sklearn.metrics import r2_score
 
+df = pd.read_csv("results/data.csv")
+
 
 # Aminoacids
 path = "results/aminoacids"
+# 	Creating the target directory
 if os.path.isdir(path):
 	print(f"The directory {path} exists already")
 else:
@@ -19,9 +22,8 @@ else:
 		quit()
 	else:
 		print("Successfully created the directory %s" % path)
-df = pd.read_csv("results/data.csv")C
-
 print("Plotting Aminoacids (x23)")
+# 	List of aminoacids (columns of the df)
 aminoacids = [
 			"PhePer",
 			"LeuPer",
@@ -44,12 +46,18 @@ aminoacids = [
 			"ArgPer",
 			"GlyPer"
 			]
+# 	Setting the common x
 x = df["IntergenomicGC"]
 for aminoacid in aminoacids:
-	y = df[aminoacid]    
+	y = df[aminoacid]
+	# Calculating the values to show in the label
 	slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
 	R_square = r2_score(x, y)
-	sn.regplot(x=x, y=y, line_kws={'label':"y = {0:.2f}+{1:.2f}x | R {2:.2f} | R squared {3:.2f}".format(intercept, slope, r_value,R_square)})
+	# Plotting the graph and the label
+	sn.regplot(x=x, y=y, line_kws={'label':"y = {0:.2f}+{1:.2f}x | R {2:.2f} | R squared {3:.2f}".format(intercept, slope, r_value, po
+	R_square)})
 	plt.legend()
+	# Saving in the predetermined path
 	plt.savefig(path + "/" + aminoacid + ".jpg")
+	# Getting ready for the next plot
 	plt.close("all")
